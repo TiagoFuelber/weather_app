bed6ef90c34946d53602b0823e307fc9ff2ff45c
wplay2 <web2@wplay.com.br>
2018-04-06 17:45:16 -0300

    fixes linter errors

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-console.log('entrou aqui');
+import { API_URL } from './Constants';
 
-var apiLink = "https://api.apixu.com/v1/current.json?key=d0f7e0df0b704466a40162720160911&q=",
-    form = $('#form'),
-    obj;
-
+const form = $('#form');
+var obj;
 
 // declares the variables with the background images
 var sunpic = "url('http://static.tumblr.com/dpm5r31/NS3og39pj/suny_day.jpeg')";
@@ -14,68 +12,72 @@ var snowpic = "url('http://static.tumblr.com/dpm5r31/wSyogft9q/neve.jpg')";
 var rainpic = "url('http://static.tumblr.com/dpm5r31/abGogft9c/chuva.jpg')";
 var clearpic = "url('http://static.tumblr.com/dpm5r31/W0Rogft8y/ceu-limpo.jpg')";
 
-function background() {
-    switch (obj.current.condition.code) {
-      case 1000:
-      default:
-        $('body').css('background-image', clearpic);
-        break;
-      case 1003:
-        $('body').css('background-image', sunpic);
-        break;
-      case 1006: case 1009:
-        $('body').css('background-image', cloudypic);
-        break;
-      case 1030: case 1135: case 1147:
-        $('body').css('background-image', mistypic);
-        break;
-      case 1063: case 1069: case 1072: case 1150: case 1153: case 1168: case 1171: case 1180: case 1183: case 1186: case 1189: case 1192: case 1195: case 1198: case 1201: case 1240: case 1243: case 1246: case 1273: case 1276:
-        $('body').css('background-image', rainpic);
-        break;
-      case 1066: case 1114: case 1117: case 1204: case 1207: case 1210: case 1213: case 1216: case 1219: case 1222: case 1225: case 1237: case 1249: case 1252: case 1255: case 1258: case 1261: case 1264: case 1279: case 1282:
-        $('body').css('background-image', snowpic);
-        break;
-      case 1087:
-        $('body').css('background-image', thunderpic);
-        break;
-       }
-  }
+function background(obj) {
+  switch (obj.current.condition.code) {
+    case 1000:
+    default:
+      $('body').css('background-image', clearpic);
+      break;
+    case 1003:
+      $('body').css('background-image', sunpic);
+      break;
+    case 1006: case 1009:
+      $('body').css('background-image', cloudypic);
+      break;
+    case 1030: case 1135: case 1147:
+      $('body').css('background-image', mistypic);
+      break;
+    case 1063: case 1069: case 1072: case 1150: case 1153: case 1168: case 1171: case 1180: case 1183: case 1186: case 1189: case 1192: case 1195: case 1198: case 1201: case 1240: case 1243: case 1246: case 1273: case 1276:
+      $('body').css('background-image', rainpic);
+      break;
+    case 1066: case 1114: case 1117: case 1204: case 1207: case 1210: case 1213: case 1216: case 1219: case 1222: case 1225: case 1237: case 1249: case 1252: case 1255: case 1258: case 1261: case 1264: case 1279: case 1282:
+      $('body').css('background-image', snowpic);
+      break;
+    case 1087:
+      $('body').css('background-image', thunderpic);
+      break;
+  };
+};
 
 function apiUrl(position) {
-    var apiCall = apiLink + position.coords.latitude + "," + position.coords.longitude;
+  var apiCall = API_URL + position.coords.latitude + "," + position.coords.longitude;
   // requests the JSON from the API
   $.getJSON(apiCall, function(json) {
-       obj = JSON.stringify(json);
-       obj = JSON.parse(obj);
-       showContent();
-       });
+    var obj = JSON.stringify(json);
+    obj = JSON.parse(obj);
+    showContent(obj);
+  });
 }
 
 // Shows weather information on page
-function showContent() {
+function showContent(obj) {
   $('#city').html(obj.location.name);
   $('#temp').html(obj.current.temp_c + "°C");
   $('#description').html(obj.current.condition.text);
   $('#weather-icon').attr("src", obj.current.condition.icon);
-  background();
+  background(obj);
 }
+
 // gets the user coordinates and stores in apiCall
-    if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(apiUrl)
-    } else {
-        window.alert("Geolocation not working");
-    }
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(apiUrl)
+} else {
+    window.alert("Geolocation not working");
+}
+
 // Button search
 form.on('submit', function (e) {
   e.preventDefault();
-  var x = apiLink + $('#inputCity').val();
+  var x = API_URL + $('#inputCity').val();
+
   $.getJSON(x, function(json) {
-       obj = JSON.stringify(json);
-       obj = JSON.parse(obj);
-       //$('#city').html(obj);
-       showContent();
-       });
+    obj = JSON.stringify(json);
+    obj = JSON.parse(obj);
+    //$('#city').html(obj);
+    showContent();
+    });
 });
+
 $(document).ready(function() {
   // switch button
   $('#switchBtn').click(function() {
